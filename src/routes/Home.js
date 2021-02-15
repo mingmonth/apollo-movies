f01c2df38e1519b36f6e47a3faf56641772629e0
김윤수 <ys9922@naver.com>
2021-02-15 19:28:53 +0900

    #1.1 GET MOVIES Query (05:37)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,21 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
